a3345588c317bdb996eb73fa5454b8ce1aa785b3
rifki <mhmdrfki920@gmail.com>
2024-10-02 14:01:19 +0700

    merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,8 @@ console.log(add(1,2));
 const subtract =(a, b) => a - b;
 console.log(subtract(5,2));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(2,2));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(2,2));
 
 const devide = (a, b) => a / b;
 console.log(devide(4,2));
