872ca8da15fa8ccd87242ec94a43e09a0a380533
rifki <mhmdrfki920@gmail.com>
2024-10-02 13:42:49 +0700

    menambahkan fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -1,6 +1,8 @@
 const add = (a, b) => a + b;
-console.log(add(1,2))
+console.log(add(1,2));
 
 const subtract =(a, b) => a - b;
-console.log(subtract(5,2))
+console.log(subtract(5,2));
 
+const multiply = (a, b) => a * b;
+console.log(multiply(2,2));
